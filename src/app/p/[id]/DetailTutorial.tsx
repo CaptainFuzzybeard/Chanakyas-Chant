200f019982bc6299b6793a5c427906f281ed2a15
CaptainFuzzybeard <bhanu.garg1997@gmail.com>
2026-04-09 08:13:01 +0530

    Update code

--- src/app/p/[id]/DetailTutorial.tsx
+++
@@ -1,5 +0,0 @@
-'use client'
-import { FirstTimeTutorial } from '@/components/ui/FirstTimeTutorial'
-export function DetailTutorial() {
-  return <FirstTimeTutorial page="politician-detail" visible={true} onDone={() => {}} />
-}
